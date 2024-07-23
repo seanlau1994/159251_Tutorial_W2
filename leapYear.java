849f853fbbfed0c50fc91d409a848bb848a9047e
seanlau1994 <seanlau1994@gmail.com>
2024-07-23 23:50:03 +1200

    Added feature 3, print year in bold and red

--- leapYear.java
+++ leapYear.java
@@ -5,15 +5,19 @@ public class leapYear{
  
         public static void main(String[] args) {
                
+                // ANSI escape codes for bold and red text
+                final String ANSI_BOLD_RED = "\033[1;31m";
+                final String ANSI_RESET = "\033[0m";
+
                 //year we want to check
                 int year = 2004;
-               
+
                 //if year is divisible by 4, it is a leap year
                
                 if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
-                        System.out.println("Year " + year + " is a leap year");
+                        System.out.println("Year " + ANSI_BOLD_RED + year + ANSI_RESET + " is a leap year");
                 else
-                        System.out.println("Year " + year + " is not a leap year");
+                        System.out.println("Year " + ANSI_BOLD_RED + year + ANSI_RESET + " is not a leap year");
         }
 }
  
